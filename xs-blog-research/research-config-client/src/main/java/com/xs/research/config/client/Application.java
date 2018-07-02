4c008e700d63688e50fe59c18f7740f32d876d4e
xiongshun <xiongshun@wecash.net>
2018-07-02 22:14:17 +0800

    ha

---
+++ xs-blog-research/research-config-client/src/main/java/com/xs/research/config/client/Application.java
@@ -0,0 +1,18 @@
+package com.xs.research.config.client;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by xs on 2018/7/2
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+}
