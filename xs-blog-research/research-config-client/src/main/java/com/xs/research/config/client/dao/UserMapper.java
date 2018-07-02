8d0a9adf20bd2a7558c5680df2575018ff5bc8fc
xiongshun <xs6666@qq.com>
2018-07-02 23:30:11 +0800

    research

---
+++ xs-blog-research/research-config-client/src/main/java/com/xs/research/config/client/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.xs.research.config.client.dao;
+
+import com.xs.research.config.client.bean.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
